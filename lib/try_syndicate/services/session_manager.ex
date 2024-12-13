43ec2c6d86ae6f0ed0b2e4d19b7abc28afbabb2f
Sam Caldwell <samcaldwell19@gmail.com>
2024-12-13 10:59:05 -0500

    add some error handling

--- lib/try_syndicate/services/session_manager.ex
+++ lib/try_syndicate/services/session_manager.ex
@@ -13,25 +13,40 @@ defmodule TrySyndicate.SessionManager do
     GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
   end
 
+  @spec start_session() :: {:ok, String.t()} | {:error, String.t()}
   def start_session() do
     session_id = UUID.uuid4()
-    Logger.info("Starting session: #{session_id}")
-    :ok = start_repl_session(session_id)
-    GenServer.cast(__MODULE__, {:add_session, session_id})
-    session_id
+
+    case GenServer.call(__MODULE__, {:start_session, session_id}) do
+      :ok -> {:ok, session_id}
+      {:error, reason} -> {:error, reason}
+    end
   end
 
   def execute_code(session_id, code) do
     url = "#{@sandbox_url}/submit"
     body = Jason.encode!(%{session_id: session_id, code: code})
     headers = [{"Content-Type", "application/json"}]
+
     case Finch.build(:post, url, headers, body) |> Finch.request(TrySyndicate.Finch) do
-      {:ok, %Finch.Response{body: body}} -> case Jason.decode(body) do
-        {:ok, %{"status" => "ok", "result" => output}} -> {:ok, output}
-        {:ok, %{"status" => reason}} -> {:error, reason}
-        {:error, reason} -> {:error, reason}
-      end
-      {:error, reason} -> {:error, reason}
+      {:ok, %Finch.Response{body: body}} ->
+        case Jason.decode(body) do
+          {:ok, %{"status" => "ok", "result" => output}} -> {:ok, output}
+          {:ok, %{"status" => reason}} -> {:error, reason}
+          {:error, reason} -> {:error, reason}
+        end
+
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  def handle_call({:start_session, session_id}, _from, state) do
+    Logger.info("Starting session: #{session_id}")
+
+    case start_repl_session(session_id) do
+      :ok -> {:reply, :ok, Map.put(state, session_id, :active)}
+      {:error, reason} -> {:reply, {:error, reason}, state}
     end
   end
 
@@ -39,13 +54,10 @@ defmodule TrySyndicate.SessionManager do
     url = "#{@sandbox_url}/new"
     body = Jason.encode!(%{session_id: session_id})
     headers = [{"Content-Type", "application/json"}]
+
     case Finch.build(:post, url, headers, body) |> Finch.request(TrySyndicate.Finch) do
       {:ok, %Finch.Response{status: 200}} -> :ok
       {:error, reason} -> {:error, reason}
     end
-  end
-
-  def handle_cast({:add_session, session_id}, state) do
-    {:noreply, Map.put(state, session_id, :active)}
   end
 end
--- lib/try_syndicate_web/live/editor_live.ex
+++ lib/try_syndicate_web/live/editor_live.ex
@@ -6,29 +6,54 @@ defmodule TrySyndicateWeb.EditorLive do
 
   def mount(_params, _session, socket) do
     if connected?(socket) do
-      session_id = SessionManager.start_session()
-      TrySyndicateWeb.Endpoint.subscribe("session:#{session_id}")
-      {:ok, assign(socket, session_id: session_id, submissions: [], program_output: "", program_error: "")}
+      case SessionManager.start_session() do
+        {:error, reason} ->
+          next_sock =
+            put_flash(socket, :error, "Failed to start session: #{inspect(reason)}")
+            |> assign(session_id: nil, submissions: [], program_output: "", program_error: "")
+
+          {:ok, next_sock}
+
+        {:ok, session_id} ->
+          TrySyndicateWeb.Endpoint.subscribe("session:#{session_id}")
+
+          {:ok,
+           assign(socket,
+             session_id: session_id,
+             submissions: [],
+             program_output: "",
+             program_error: ""
+           )}
+      end
     else
-      {:ok, assign(socket, submissions: [], program_output: "", program_error: "")}
+      {:ok, assign(socket, session_id: nil, submissions: [], program_output: "", program_error: "")}
     end
   end
 
   def handle_event("run_code", %{"code" => code}, socket) do
-    case SessionManager.execute_code(socket.assigns.session_id, code) do
-      {:ok, result} ->
-        submissions = socket.assigns.submissions ++ [%{code: code, output: result}]
-        {:noreply, assign(socket, output: "", submissions: submissions)}
-      {:error, reason} -> {:noreply, assign(socket, :output, reason)}
+    case Map.get(socket.assigns, :session_id) do
+      nil ->
+        {:noreply, put_flash(socket, :error, "Session not started")}
+
+      session_id ->
+        case SessionManager.execute_code(session_id, code) do
+          {:ok, result} ->
+            submissions = socket.assigns.submissions ++ [%{code: code, output: result}]
+            {:noreply, assign(socket, output: "", submissions: submissions)}
+
+          {:error, reason} ->
+            {:noreply, put_flash(socket, :error, "Error running code: #{reason}")}
+        end
     end
   end
 
   def handle_info(%{event: "update", payload: %{type: update_type, data: update_data}}, socket) do
-    key = case update_type do
-      "stdout" -> :program_output
-      "stderr" -> :program_error
-    end
+    key =
+      case update_type do
+        "stdout" -> :program_output
+        "stderr" -> :program_error
+      end
+
     {:noreply, update(socket, key, fn existing -> existing <> update_data end)}
   end
-
 end
