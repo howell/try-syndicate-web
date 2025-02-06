c79b5a339bea3ee53b066ab5bba5acd25b934e43
Sam Caldwell <samcaldwell19@gmail.com>
2025-02-06 14:01:55 -0500

    draw a box for incoming event and an arrow to the receiving actor

--- lib/try_syndicate_web/live/dataspace_component.ex
+++ lib/try_syndicate_web/live/dataspace_component.ex
@@ -25,17 +25,22 @@ defmodule TrySyndicateWeb.DataspaceComponent do
     }
   end
 
+  def event_green(), do: "#50d979"
+  def event_opacity(), do: "0.75"
+
   attr :dataspace, Dataspace, required: true
 
   def dataspace(assigns) do
     the_dims = dims()
-    {layout_actors, actor_height} = sort_and_layout_actors(assigns.dataspace.actors, the_dims)
+
+    {layout_actors, actor_height} =
+      sort_and_layout_actors(assigns.dataspace.actors, assigns.dataspace.active_actor, the_dims)
 
     {layout_actions, actions_height} =
       compute_pending_actions_layout(assigns.dataspace.pending_actions, the_dims)
 
     assigns
-    |> assign(:svg_height, svg_height(assigns.dataspace, the_dims, actor_height, actions_height))
+    |> assign(:svg_height, svg_height(the_dims, actor_height, actions_height))
     |> assign(:dims, the_dims)
     |> assign(:actor_height, actor_height)
     |> assign(:layout_actors, layout_actors)
@@ -53,6 +58,18 @@ defmodule TrySyndicateWeb.DataspaceComponent do
   def render_dataspace_with_dims(assigns) do
     ~H"""
     <svg id="ds_diagram" width="100%" height={@svg_height}>
+      <defs>
+        <marker
+          id="arrowhead-green-down"
+          markerWidth="6"
+          markerHeight="6"
+          refX="3"
+          refY="0"
+          orient="0"
+        >
+          <polygon points="0 0, 3 6, 6 0" fill={event_green()} />
+        </marker>
+      </defs>
       <g id="actors">
         <%= for {id, actor, layout} <- @layout_actors do %>
           <.actor_box
@@ -75,10 +92,10 @@ defmodule TrySyndicateWeb.DataspaceComponent do
     """
   end
 
-  @spec sort_and_layout_actors(%{Dataspace.actor_id() => Actor.t()}, map()) ::
+  @spec sort_and_layout_actors(%{Dataspace.actor_id() => Actor.t()}, term(), map()) ::
           {[{Dataspace.actor_id(), Actor.t(), actor_layout()}], integer()}
-  def sort_and_layout_actors(actors, dims) do
-    {actors, height} = compute_actor_layout(actors, dims)
+  def sort_and_layout_actors(actors, active_actor, dims) do
+    {actors, height} = compute_actor_layout(actors, active_actor, dims)
     {Enum.sort_by(actors, fn {_, _, layout} -> layout.c_y end), height}
   end
 
@@ -141,7 +158,7 @@ defmodule TrySyndicateWeb.DataspaceComponent do
         y={@layout.c_y}
         width={@dims.actor_box_width}
         height={@dims.actor_box_height}
-        fill={if is_active?(@id, @active), do: "#50d979", else: "#eef"}
+        fill={if is_active?(@id, @active), do: event_green(), else: "#eef"}
         opacity=".75"
         stroke="#333"
         rx="5"
@@ -166,12 +183,39 @@ defmodule TrySyndicateWeb.DataspaceComponent do
         stroke="black"
       />
       <.assertions_box assertions={@actor.assertions} layout={@layout} dims={@dims} />
+      <.boxed_actions
+        :if={is_active?(@id, @active)}
+        x={@dims.dataspace_box_x + @dims.assertions_box_x_offset}
+        y={@layout.c_y - @layout.event_height - @dims.vertical_padding}
+        actions={[elem(@active, 1)]}
+        width={@dims.assertions_box_width}
+        height={@layout.event_height}
+        opacity={event_opacity()}
+        fill={event_green()}
+      />
+      <polyline
+        :if={is_active?(@id, @active)}
+        points={event_to_actor_points(@dims, @layout)}
+        stroke={event_green()}
+        fill="none"
+        stroke-width="2"
+        marker-end="url(#arrowhead-green-down)"
+      />
     </g>
     """
   end
 
   def is_active?(actor_id, active_actor) do
     active_actor != :none && elem(active_actor, 0) == actor_id
+  end
+
+  defp event_to_actor_points(dims, layout) do
+    event_box_x = dims.dataspace_box_x + dims.assertions_box_x_offset
+    event_box_mid_y = layout.c_y - layout.event_height / 2 - dims.vertical_padding
+    actor_mid_x = dims.actor_x_offset + dims.actor_box_width / 2
+    before_actor_id_y = layout.c_y - 28
+
+    "#{event_box_x},#{event_box_mid_y} #{actor_mid_x},#{event_box_mid_y} #{actor_mid_x},#{before_actor_id_y}"
   end
 
   attr :dims, :map, required: true
@@ -254,16 +298,37 @@ defmodule TrySyndicateWeb.DataspaceComponent do
         <%= @origin.space %>
       </text>
 
+      <.boxed_actions
+        actions={@actions}
+        width={@dims.pending_actions_box_width}
+        height={@layout.height}
+      />
+    </g>
+    """
+  end
+
+  attr :x, :integer, required: false, default: 0
+  attr :y, :integer, required: false, default: 0
+  attr :width, :integer, required: true
+  attr :height, :integer, required: true
+  attr :actions, :list, required: true
+  attr :fill, :string, required: false, default: "none"
+  attr :opacity, :string, required: false, default: "1"
+
+  def boxed_actions(assigns) do
+    ~H"""
+    <g transform={"translate(#{@x}, #{@y})"}>
       <rect
         x={0}
         y={0}
-        width={@dims.pending_actions_box_width}
-        height={@layout.height}
-        fill="none"
+        width={@width}
+        height={@height}
+        fill={@fill}
+        opacity={@opacity}
         stroke="black"
         stroke-width="1"
       />
-      <foreignObject width={@dims.pending_actions_box_width} height={@layout.height}>
+      <foreignObject width={@width} height={@height}>
         <div xmlns="http://www.w3.org/1999/xhtml" class="text-left text-xs overflow-auto p-2">
           <ul class="gap-y-2 divide-y divide-slate-300 divide-dashed">
             <%= for action <- @actions do %>
@@ -277,7 +342,7 @@ defmodule TrySyndicateWeb.DataspaceComponent do
   end
 
   # Calculate the height of the SVG based on the number of actors and states
-  def svg_height(ds, dims, actor_height, actions_height) do
+  def svg_height(dims, actor_height, actions_height) do
     max(
       actor_height + 2 * dims.vertical_padding,
       actions_height + 2 * dims.vertical_padding
@@ -335,9 +400,9 @@ defmodule TrySyndicateWeb.DataspaceComponent do
           block_height: integer()
         }
 
-  @spec compute_actor_layout(%{Dataspace.actor_id() => Actor.t()}, map()) ::
+  @spec compute_actor_layout(%{Dataspace.actor_id() => Actor.t()}, term(), map()) ::
           {[{Dataspace.actor_id(), Actor.t(), actor_layout()}], integer()}
-  def compute_actor_layout(actors, dims) do
+  def compute_actor_layout(actors, active_actor, dims) do
     actor_box_height = dims[:actor_box_height]
     state_item_height = dims[:assertions_box_height]
     assertions_box_padding = dims[:assertions_box_padding]
@@ -348,20 +413,29 @@ defmodule TrySyndicateWeb.DataspaceComponent do
       assertions_box_height =
         assertions_box_padding * 2 + max(length(actor.assertions), 1) * state_item_height
 
+      event_height =
+        if is_active?(id, active_actor),
+          do:
+            height_for_actions([elem(active_actor, 1)], dims.action_height, dims.action_padding),
+          else: 0
+
+      event_space = if event_height > 0, do: event_height + vertical_padding, else: 0
+
       actor_height = actor_box_height
 
-      block_height = max(actor_box_height, assertions_box_height)
-      c_y = y_offset + (block_height - actor_height) / 2
-      s_y = y_offset + (block_height - assertions_box_height) / 2
+      block_height = max(actor_box_height, assertions_box_height + event_space)
+      c_y = y_offset + event_height + (block_height - actor_height) / 2
+      s_y = y_offset + event_height + (block_height - assertions_box_height) / 2
 
       layout = %{
         c_y: c_y,
         s_y: s_y,
         assertions_box_height: assertions_box_height,
+        event_height: event_height,
         block_height: block_height
       }
 
-      {{id, actor, layout}, y_offset + block_height + vertical_spacing}
+      {{id, actor, layout}, y_offset + block_height + vertical_spacing + vertical_padding}
     end)
   end
 
@@ -375,15 +449,16 @@ defmodule TrySyndicateWeb.DataspaceComponent do
     vertical_padding = dims[:vertical_padding]
 
     Enum.map_reduce(pending_actions, vertical_padding, fn {st, actions}, y_offset ->
-      action_lines = Enum.sum(Enum.map(actions, &lines_for_action/1))
-
-      height =
-        action_height * max(1, action_lines) + 2 * action_padding +
-          action_padding * length(actions)
+      height = height_for_actions(actions, action_height, action_padding)
 
       layout = %{y: y_offset, height: height}
 
       {{st, actions, layout}, y_offset + height}
     end)
+  end
+
+  def height_for_actions(actions, line_height, padding) do
+    action_lines = Enum.sum(Enum.map(actions, &lines_for_action/1))
+    line_height * max(1, action_lines) + 2 * padding + padding * max(0, length(actions) - 1)
   end
 end
