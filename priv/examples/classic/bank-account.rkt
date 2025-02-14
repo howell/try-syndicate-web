80960f0b02542dc413ec8173343a3670f50d5a65
Sam Caldwell <samcaldwell19@gmail.com>
2025-02-13 19:51:31 -0500

    add actor names to bank-account

--- priv/examples/classic/bank-account.rkt
+++ priv/examples/classic/bank-account.rkt
@@ -4,14 +4,17 @@
 (struct account (balance) #:prefab)
 (struct deposit (amount) #:prefab)
 
-(spawn (field [balance 0])
+(spawn #:name 'banker
+       (field [balance 0])
        (assert (account (balance)))
        (on (message (deposit $amount))
            (balance (+ (balance) amount))))
 
-(spawn (on (asserted (account $balance))
+(spawn #:name 'observer
+       (on (asserted (account $balance))
            (printf "Balance changed to ~a\n" balance)))
 
-(spawn* (until (asserted (observe (deposit _))))
+(spawn* #:name 'client
+        (until (asserted (observe (deposit _))))
         (send! (deposit +100))
         (send! (deposit -30)))
