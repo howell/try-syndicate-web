5d7b0b7900369d476662486810f16bf377ef7ea9
Sam Caldwell <samcaldwell19@gmail.com>
2025-02-20 21:28:07 -0500

    parsing adjustment

--- lib/try_syndicate/syndicate/json.ex
+++ lib/try_syndicate/syndicate/json.ex
@@ -1,15 +1,17 @@
 defmodule TrySyndicate.Syndicate.Json do
-  @spec parse_field(map(), String.t(), (any() -> bool())) :: {:ok, any()} | {:error, String.t()}
-  def parse_field(json, field, validator \\ fn _ -> true end) do
+  @type parser() :: (term() -> {:ok, term()} | {:error, String.t()})
+
+  @spec parse_field(map(), String.t()) :: {:ok, any()} | {:error, String.t()}
+  @spec parse_field(map(), String.t(), parser()) :: {:ok, any()} | {:error, String.t()}
+  def parse_field(json, field, validator \\ fn v -> {:ok, v} end) do
     case Map.get(json, field) do
       nil ->
         {:error, "Missing field: #{field}"}
 
       value ->
-        if validator.(value) do
-          {:ok, value}
-        else
-          {:error, "Invalid field: #{field}"}
+        case validator.(value) do
+          {:ok, value} -> {:ok, value}
+          {:error, reason} -> {:error, "Invalid value for #{field}: #{reason}"}
         end
     end
   end
@@ -34,5 +36,4 @@ defmodule TrySyndicate.Syndicate.Json do
       {:ok, json}
     end
   end
-
 end
